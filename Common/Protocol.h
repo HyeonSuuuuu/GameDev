ee67baf76fa14a8d95323dfc72f2b6280d91687b
HyeonSu <ih9158228@gmail.com>
2026-03-25 17:44:55 +0900

    protocol에 ErroCode 추가

--- Common/Protocol.h
+++ Common/Protocol.h
@@ -66,6 +66,18 @@ enum class PACKET_ID : uint16
 
 };
 
+enum class ERROR_CODE : uint8
+{
+	NONE = 0,
+
+	// User Manager
+	USER_LOGIN_USED_ALL = 11,
+	/*	USER_MGR_INVALID_INDEX = 11,
+		USER_MGR_INVALID_USER_UNIQUE_ID = 12,*/
+
+		// Login
+};
+
 #pragma pack(push, 1)
 struct PacketHeader
 {
