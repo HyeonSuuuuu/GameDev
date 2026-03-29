df31e2093c3ceb40681f1cc1f5471988e380c2c9
polarune(desktop) <jumpcross123@naver.com>
2026-03-29 17:49:02 +0900

    로그인 ui 약간 수정, 로그인버튼 누르면 다음 레벨로 이동하게 변경

--- Client/TurtleIsland/Source/TurtleIsland/LoginWidget.cpp
+++ Client/TurtleIsland/Source/TurtleIsland/LoginWidget.cpp
@@ -4,11 +4,13 @@
 #include "LoginWidget.h"
 #include "Network/NetSubsystem.h"
 #include "Protocol.h"
+#include "Kismet/GameplayStatics.h"
 
 void ULoginWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
-	// 블루프린트의 버튼과 C++ 포인터가 잘 연결되었다면 클릭 이벤트 등록!
+
+
 	if (LoginButton)
 	{
 		LoginButton->OnClicked.AddDynamic(this, &ULoginWidget::OnLoginButtonClicked);
@@ -17,18 +19,19 @@ void ULoginWidget::NativeConstruct()
 
 void ULoginWidget::OnLoginButtonClicked()
 {
-	// 여기서 서브시스템의 큐에 데이터를 바로 던집니다.
-	if (UNetSubsystem* NetSubsystem = GetGameInstance()->GetSubsystem<UNetSubsystem>())
+	// 입력된 텍스트 가져오기
+	FString IDString = UserIDInput->GetText().ToString();
+	FString PWString = UserPWInput->GetText().ToString();
+
+	// 유효성 검사: 비어있거나 공백만 있는지 확인
+	if (IDString.TrimStartAndEnd().IsEmpty() || PWString.TrimStartAndEnd().IsEmpty())
 	{
-		CS_Login LoginPacket {};
-		LoginPacket.size = CS_LOGIN_PACKET_SIZE;
-		LoginPacket.id = (uint16)PACKET_ID::CS_LOGIN_REQUEST;
-		// 버퍼에 담기
-		TArray<uint8> SendBuffer;
-		SendBuffer.SetNumUninitialized(CS_LOGIN_PACKET_SIZE);
-		FMemory::Memcpy(SendBuffer.GetData(), &LoginPacket, CS_LOGIN_PACKET_SIZE);
-		
-		NetSubsystem->EnqueSendPacket(MoveTemp(SendBuffer));
-		UE_LOG(LogTemp, Log, TEXT("C++에서 버튼 클릭을 감지하여 큐에 넣었습니다!"));
+		UE_LOG(LogTemp, Warning, TEXT("ID 또는 PW를 입력해주세요."));
+
+		// 사용자에게 알림을 주기 위해 UI에 경고 문구를 띄우는 로직을 여기에 추가할 수 있습니다.
+
+		return; // 입력이 잘못되었으므로 아래 로직(패킷 전송 및 레벨 이동)을 실행하지 않고 종료
 	}
+
+	else (UGameplayStatics::OpenLevel(GetWorld(), FName("Lv1_Lobby")));
 }
